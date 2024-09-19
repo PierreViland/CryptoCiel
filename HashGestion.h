d030a327446b06cec63671c755568bf1011ccec5
Megaghosty <153129021+Megaghosty@users.noreply.github.com>
2024-09-19 12:28:31 +0200

    update

--- HashGestion.h
+++ HashGestion.h
@@ -1,10 +1,3 @@
-/*****************************************************************//**
- * \file   HashGestion.h
- * \brief  Classe permettant de faire du SHA256 sur des fichiers ou des chaines de caractères
- * 
- * \author Pierre
- * \date   July 2024
- *********************************************************************/
 #pragma once
 
 #include <iostream>
--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -5,13 +5,32 @@
 #include "RsaGestion.h"
 #include "Hashgestion.h"
 #include "AesGestion.h"
+#include <fstream>
 
 int main()
 {
 	HashGestion LM;
-	std::string monMessageHash = "Super message superchouette";
-	std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
+	std::string File = "test.txt";
+	std::cout << LM.CalculateFileSHA256(File) << std::endl;
+	
 
+
+	AesGestion Cia;
+	Cia.GenerateAESKey();
+	Cia.SaveAESKeyToFile("Cia_code.key");
+	Cia.EncryptFileAES256("test.txt", "testencrypt.txt");
+	Cia.DecryptFileAES256("testencrypt.txt", "testdecrypt.txt");
+
+	RsaGestion Fbi;
+	Fbi.generationClef("clef_pub.pem", "clef_pr.pem",2048);
+	Fbi.chargementClefsPrive("clef_pr.pem");
+	Fbi.chargementClefsPublic("clef_pub.pem");
+	Fbi.chiffreDansFichier("message", "test1.txt");
+	
+	std::string micode = "test1.txt";
+	std::cout << Fbi.dechiffreFichier(micode) << std::endl;
+	
+
+	return 0;
+	
 }
-
-
